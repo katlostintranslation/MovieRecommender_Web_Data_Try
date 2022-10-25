251056142b1852bb6ac8c62f05d31b962d8a147b
KK <katrinakraukle.kk@gmail.com>
2022-10-25 18:50:21 +0300

    before trying to merge console app

--- MovieRecommender2022.Data/Models/Movie.cs
+++ MovieRecommender2022.Data/Models/Movie.cs
@@ -9,35 +9,35 @@ namespace MovieRecommender2022.Data.Models
 {
     public class Movie
     {
-        [Key]
-        public int Rating { get; set; }
-       
-        public string Title { get; set; }
-        [Required]
-        [MaxLength(50)]
-        public string Genre { get; set; }
-       
-
-        //public Movie(string title)
-        //{
-        //    Title = title;
-        //}
-
-        //public Movie()
-        //{
-        //}
-
-        //public string Title { get; set; }
-
-        //public GenreEnum Genre { get; set; }
-
-        //public string[] Keywords { get; set; }
-
+        //[Key]
         //public int Rating { get; set; }
+       
+        //public string Title { get; set; }
+        //[Required]
+        //[MaxLength(50)]
+        //public string Genre { get; set; }
 
-        //public override string ToString()
-        //{
-        //    return $"{Title}\n{Genre}";
-        //}
+
+        public Movie(string title)
+        {
+            Title = title;
+        }
+
+        public Movie()
+        {
+        }
+
+        public string Title { get; set; }
+
+        public GenreEnum Genre { get; set; }
+
+        public string[] Keywords { get; set; }
+
+        public int Rating { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}\n{Genre}";
+        }
     }
 }
