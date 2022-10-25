a57f708930d90a3880e6425b6e913bdd2fe5d907
KK <katrinakraukle.kk@gmail.com>
2022-10-25 19:03:58 +0300

    2ndattempt

---
+++ MovieRecommender2022.Web/MovieRecFunctions/FindRecommendationHelper.cs
@@ -0,0 +1,116 @@
+﻿using MovieRecommender2022.Data;
+using MovieRecommender2022.Data.Models;
+
+namespace MovieRecommender2022.Web.MovieRecFunctions
+{
+    internal class FindRecommendationHelper
+    {
+        public static void Start(MovieList movieList)
+        {
+            var movies = movieList.Movies;
+            SearchResults(Search(movies)); //method for showing results. We let method to complete itself, so we dont need return
+        }
+
+        private static IEnumerable<Movie> Search(List<Movie> movies)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Movie> Search(IEnumerable<Movie> movies)
+        {
+            Console.WriteLine("Which category would you like to search in?");
+            var option = GetSearch();
+            Console.Write("Enter a query: ");
+            var query = Console.ReadLine();
+
+            var result = Enumerable.Empty<Movie>(); //we dont have to asign any value
+
+            if (query != null)
+            {
+                switch (option)
+                {
+                    case "T":
+                        result = SearchTitle(query, movies); //adjusted data types (use Enumerable)
+                        break;
+                    case "G":
+                        result = SearchGenre(query, movies);
+                        break;
+                    case "K":
+                        result = SearchKeyword(query, movies);
+                        break;
+                }
+            }
+
+            if (!result.Any()) //we dont need to know specific number of results and works quicker than Count
+            {
+                Console.WriteLine("Nothing found");
+            }
+            return result;
+        }
+
+        public static void SearchResults(IEnumerable<Movie> results)
+        {
+            Console.WriteLine("Results:"); //we print out movies. Still says Results when nothing is found.
+            //for (int i = 0; i <results.Count(); i++)
+            //{
+            //    Console.WriteLine($"{i + 1}. {results.ElementAt(i).Title}");
+            //}
+
+            int j = 1; //initial value
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{j}. {item.Title}");
+                ++j;
+            }
+        }
+
+        private static string GetSearch()
+        {
+            while (true)
+            {
+                Console.WriteLine(Menu.Dash);
+                Console.WriteLine("1 - Title [T]");
+                Console.WriteLine("2 - Keywords [K]");
+                Console.WriteLine("3 - Genre [G]");
+                Console.WriteLine(Menu.Dash);
+
+                Console.WriteLine("Select: ");
+
+                var input = Console.ReadLine();
+
+                if (input != null && SearchValidation(input))
+                {
+                    return input.ToUpper();
+                }
+            }
+        }
+
+        //private static string GetSearch()
+        //{
+        //    while (true)
+        //    {
+        //        Console.WriteLine(Menu.Dash);
+        //    }
+        //}
+        private static bool SearchValidation(string userInput)
+        {
+            var validMenuSelection = new string[] { "T", "K", "G" };
+
+            return validMenuSelection.Contains(userInput.ToUpper());
+        }
+
+        private static IEnumerable<Movie> SearchTitle(string query, IEnumerable<Movie> movies)
+        {
+            return movies.Where(x => x.Title.Contains(query, StringComparison.InvariantCultureIgnoreCase)); //linq
+        }
+        private static IEnumerable<Movie> SearchKeyword(string query, IEnumerable<Movie> movies)
+        {
+            return movies.Where(x => x.Keywords.Any(z => z.Contains(query, StringComparison.InvariantCultureIgnoreCase))); //we are looking with Any
+        }
+
+        private static IEnumerable<Movie> SearchGenre(string query, IEnumerable<Movie> movies)
+        {
+            return movies.Where(x => x.Genre.ToString().Contains(query, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
--- MovieRecommender2022.Web/Program.cs
+++ MovieRecommender2022.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MovieRecommender2022.Data;
+using MovieRecommender2022.Web.MovieRecFunctions;
 
 namespace MovieRecommender2022.Web
 {
@@ -8,6 +9,42 @@ namespace MovieRecommender2022.Web
     {
         public static void Main(string[] args)
         {
+            var movieList = new MovieList();
+
+            while (true)
+            {
+                var menuSelection = Menu.GetUserMenuSelection();
+
+                switch (menuSelection) //cause our menu is predefined
+                {
+                    case "A":
+                        AddMovieHelper.Start(movieList); //call the method
+                        break;
+                    case "D":
+                        DeleteMovieHelper.Start(movieList);
+                        break;
+                    case "F":
+                        FindRecommendationHelper.Start(movieList);
+                        break;
+                    case "E":
+                        return;
+                }
+            }
+
+
+            //movieList.Add(new Movie("Titanic"));
+            //movieList.Add(new Movie("Shrek"));
+            //movieList.Add(new Movie("Harry Potter 1"));
+            //movieList.Add(new Movie("Harry Potter 2"));
+            //movieList.Add(new Movie("Harry Potter 3"));
+            //movieList.Add(new Movie("Harry Potter 4"));
+
+            //var movies = movieList.Recommend("Titanic");
+
+            //foreach (var item in movies)
+            //{
+            //    Console.WriteLine(item);
+            //}
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
